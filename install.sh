d15187e2229ccddde5f6a25bac6c718c1d1f3700
Philipp Bliznuk <philipp.bliznuk@gmail.com>
2025-01-19 16:34:19 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -28,6 +28,28 @@ BACKUP_DIR="./backup"
 # Ensure backup directory exists
 mkdir -p "$BACKUP_DIR"
 
+# Packages to install
+packages=(
+    "alacritty"
+    "bat"
+    "git"
+    "ghostty"
+    "homebrew"
+    "kitty"
+    "nvim"
+    "ruff"
+    "starship"
+    "tmux"
+    "zsh"
+)
+# Filter the list to include only existing directories
+filtered_packages=()
+for pkg in "${packages[@]}"; do
+    if [ -d "./$pkg" ]; then
+        filtered_packages+=("$pkg")
+    fi
+done
+
 # Function to back up or remove files
 backup_or_remove() {
     local src="$1"
@@ -49,7 +71,7 @@ backup_or_remove() {
 process_dotfiles() {
     info "Processing dotfiles for backup and cleanup..."
 
-    find . -mindepth 2 -type f | while read -r file; do
+    find $(printf "./%s " "${filtered_packages[@]}") -type f | while read -r file; do
         # Get the relative path for the file
         relative_path="${file#./*/}"
 
@@ -70,14 +92,23 @@ process_dotfiles() {
 symlink_dotfiles() {
     info "Symlinking dotfiles using GNU Stow..."
 
-    find . -maxdepth 1 -mindepth 1 -type d ! -name "backup" | while read -r dir; do
-        to_stow="${dir#./}"
-        stow -v 1 ${to_stow}
+    for pkg in "${filtered_packages[@]}"; do
+        stow -v 1 ${pkg}
     done
 
     success "Dotfiles symlinked successfully."
 }
 
+# Function to install packages
+install_packages() {
+    info "Installing Brew packages"
+
+    brew bundle
+
+    success "Packages installed."
+}
+
 # Main execution
 process_dotfiles
 symlink_dotfiles
+install_packages
